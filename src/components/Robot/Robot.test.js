0bba5d7b1e73e69f7fee3b39c125b1dd49f1db6d
Menchu MV <mcmvillalobos@gmail.com>
2021-11-06 15:04:12 +0100

    Added render test to Robot

---
+++ src/components/Robot/Robot.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import Robot from "./Robot";
+import ReactTestRenderer from "react-test-renderer";
+
+describe("Given a Robot component", () => {
+  describe("When it is passed a robot object with the name 'Rogelio'", () => {
+    test("Then it should render a card with the robot's name", () => {
+      const name = "Rogelio";
+      const robot = {
+        características: {
+          velocidad: 0,
+          resistencia: 10,
+          creación: "2021",
+        },
+        _id: "61855440a99aeba4d99148ef",
+        nombre: name,
+        imagen: "https://cdn.kapwing.com/video_image-uBSRyuvqm.jpeg",
+      };
+
+      render(<Robot robot={robot} />);
+
+      const robotName = screen.getByRole("heading");
+
+      expect(robotName).toBeInTheDocument();
+    });
+  });
+});
