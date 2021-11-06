52e92a40d02a0e323deece6693b34c8095dcd5d6
Menchu MV <mcmvillalobos@gmail.com>
2021-11-06 16:49:50 +0100

    Aded loadRobotsThunk function

--- src/redux/thunks/robotsThunks.js
+++ src/redux/thunks/robotsThunks.js
@@ -1,1 +1,10 @@
+import { loadRobotsAction } from "../actions/actionCreators";
+
 const urlApi = "https://w06-robots-menchu.herokuapp.com/robots";
+
+export const loadTasksThunk = () => async (dispatch) => {
+  const response = await fetch(urlApi);
+  const tasks = await response.json();
+
+  dispatch(loadRobotsAction(tasks));
+};
