d8526de58c196352c441a4cc59fc5713db92835b
Menchu MV <mcmvillalobos@gmail.com>
2021-11-06 13:54:28 +0100

    Added some bootstrap styling to Robot card

--- src/components/Robot/Robot.js
+++ src/components/Robot/Robot.js
@@ -1,21 +1,24 @@
+import "./Robot.css";
+
 const Robot = () => {
   return (
-    <li className="robot">
-      <div className="robot__image">
+    <li className="robot card text-white bg-dark">
+      <div className="robot__image card-img-top">
         <img
+          className="robot__image-file"
           src="https://images.newscientist.com/wp-content/uploads/2021/04/21160754/21-april_pepper-the-robot.jpg"
           alt=""
           width="200"
           height="150"
         />
       </div>
-      <div className="robot__info">
-        <span className="robot__name">NOMBRE: ROGELIO</span>
+      <div className="robot__info card-body">
+        <h5 className="robot__name card-title">NOMBRE: ROGELIO</h5>
         <div class="robot__data">
-          <table>
+          <table className="table text-white">
             <thead>
               <tr>
-                <th colspan="2">CARACTERÍSTICAS</th>
+                <th scope="col">CARACTERÍSTICAS</th>
               </tr>
             </thead>
             <tbody>
