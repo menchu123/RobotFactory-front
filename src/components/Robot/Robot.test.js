8bd3cf244283feaba186de7c8b3fcda93bbfea70
Menchu MV <mcmvillalobos@gmail.com>
2021-11-06 15:05:28 +0100

    Added snapshot test

--- src/components/Robot/Robot.test.js
+++ src/components/Robot/Robot.test.js
@@ -24,4 +24,21 @@ describe("Given a Robot component", () => {
       expect(robotName).toBeInTheDocument();
     });
   });
+  describe("When it recives and object", () => {
+    test("then it should render a card with the object info inside", () => {
+      const robot = {
+        características: {
+          velocidad: 0,
+          resistencia: 10,
+          creación: "2021",
+        },
+        _id: "61855440a99aeba4d99148ef",
+        nombre: "Rogelio",
+        imagen: "https://cdn.kapwing.com/video_image-uBSRyuvqm.jpeg",
+      };
+
+      const robotCard = ReactTestRenderer.create(<Robot robot={robot} />);
+      expect(robotCard.toJSON()).toMatchSnapshot();
+    });
+  });
 });
