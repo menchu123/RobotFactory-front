2025b62d2a86638a6190c12865cf2fa531b75a3d
Menchu MV <mcmvillalobos@gmail.com>
2021-11-07 09:16:53 +0100

    Wrapped Form and RobotList test renders in store Providers

--- src/components/RobotList/RobotList.test.js
+++ src/components/RobotList/RobotList.test.js
@@ -1,13 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import { getRandomRobots } from "../../factories/robotsFactory";
 import RobotList from "./RobotList";
+import configureStore from "../../redux/store/index";
+import { Provider } from "react-redux";
 
 const robots = getRandomRobots();
 
 describe("Given a RobotList component", () => {
   describe("When it is called", () => {
     test("Then it should render a list", () => {
-      render(<RobotList robots={robots} />);
+      const store = configureStore();
+      render(
+        <Provider store={store}>
+          <RobotList robots={robots} />
+        </Provider>
+      );
 
       const robotList = screen.getByRole("list");
 
