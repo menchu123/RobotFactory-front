331227568db3955aa34b74608a025048ff733613
Menchu MV <mcmvillalobos@gmail.com>
2021-11-08 20:14:18 +0100

    Added onChange and onSubmit functionality to LoginForm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import LoginForm from "./components/Form/LoginForm/LoginForm";
+import LoginForm from "./components/LoginForm/LoginForm";
 import RobotList from "./components/RobotList/RobotList";
 
 function App() {
