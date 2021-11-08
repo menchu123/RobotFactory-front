578863f14ad1e60214975088907865562fc0bf4d
Menchu MV <mcmvillalobos@gmail.com>
2021-11-08 21:52:42 +0100

    Added onLogout function and conditional ternary expression in form to update it to Logout when loggedin

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import useUser from "../../hooks/useUser";
 
-const LoginForm = () => {
-  const { loginUser } = useUser();
+const LoginForm = ({ isAuthenticated }) => {
+  const { loginUser, logoutUser } = useUser();
   const initialValues = {
     username: "",
     password: "",
@@ -12,21 +12,29 @@ const LoginForm = () => {
   const [isDisabled, setIsDisabled] = useState(true);
 
   useEffect(() => {
-    if (userData.username !== "" && userData.password !== "") {
+    if (isAuthenticated) {
+      setIsDisabled(false);
+    } else if (userData.username !== "" && userData.password !== "") {
       setIsDisabled(false);
     } else {
       setIsDisabled(true);
     }
-  }, [userData.password, userData.username]);
+  }, [isAuthenticated, userData.password, userData.username]);
 
   const onChange = (event) => {
     setUserData({ ...userData, [event.target.id]: event.target.value });
   };
 
-  const onSubmit = (event) => {
+  const onLogin = (event) => {
     event.preventDefault();
     loginUser(userData);
     setUserData(initialValues);
+  };
+
+  const onLogout = (event) => {
+    event.preventDefault();
+    logoutUser();
+    localStorage.removeItem(process.env.REACT_APP_LOCAL_STORAGE);
   };
 
   return (
@@ -34,39 +42,46 @@ const LoginForm = () => {
       className="form-inline col-4 text-center"
       noValidate
       autoComplete="off"
-      onSubmit={onSubmit}
+      onSubmit={isAuthenticated ? onLogout : onLogin}
     >
-      <h5 className="login-title mt-3">Login</h5>
-      <div className="input-form">
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control"
-            id="username"
-            placeholder="Username"
-            value={userData.username}
-            onChange={(event) => onChange(event)}
-          />
-        </div>
-      </div>
-      <div className="input-form mt-3">
-        <div className="form-group">
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Contraseña"
-            value={userData.password}
-            onChange={(event) => onChange(event)}
-          />
-        </div>
-      </div>
+      {isAuthenticated ? (
+        ""
+      ) : (
+        <>
+          <h5 className="login-title mt-3">Login</h5>
+          <div className="input-form">
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                placeholder="Username"
+                value={userData.username}
+                onChange={(event) => onChange(event)}
+              />
+            </div>
+          </div>
+          <div className="input-form mt-3">
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Contraseña"
+                value={userData.password}
+                onChange={(event) => onChange(event)}
+              />
+            </div>
+          </div>
+        </>
+      )}
+
       <button
         type="submit"
         className="btn btn-primary mt-3"
         disabled={isDisabled}
       >
-        Login
+        {isAuthenticated ? "Logout" : "Login"}
       </button>
     </form>
   );
