7ea918c3bf69e2815da7e521a2cbe339170bc84c
Menchu MV <mcmvillalobos@gmail.com>
2021-11-08 18:59:48 +0100

    Added action types and creators

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -3,6 +3,8 @@ const actionTypes = {
   createRobot: "CREATE_ROBOT",
   deleteRobot: "DELETE_ROBOT",
   updateRobot: "UPDATE_ROBOT",
+  loginUser: "LOGIN_USER",
+  logoutUser: "LOGOUT_USER",
 };
 
 export default actionTypes;
